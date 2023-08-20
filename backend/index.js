4416355672fbc841bd83c2830cb90a7adb7953d8
t-naganuma <takuya.naganuma@gmail.com>
2023-08-20 15:11:20 +0900

    prisma settings

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
 const port = 3001;
+
+const prisma = new PrismaClient();
+app.get("/todos", async (req, res) => {
+  const todos = await prisma.todo.findMany();
+  return res.json(todos);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
